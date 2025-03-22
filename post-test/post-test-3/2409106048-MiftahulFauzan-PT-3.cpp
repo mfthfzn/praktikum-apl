a3a53cc75ed69086d7ff00df75124fdc04915adf
mfthfzn <miftahulfauzan55@gmail.com>
2025-03-22 12:06:15 +0800

    Finish Post Test 3

---
+++ post-test/post-test-3/2409106048-MiftahulFauzan-PT-3.cpp
@@ -0,0 +1,367 @@
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <string>
+using namespace std;
+
+#define MAX_SPAREPART 100
+#define MAX_USER 100
+
+struct User {
+  string username;
+  string password;
+};
+
+struct Sparepart {
+  string name;
+  int stock;
+  int price;
+};
+
+
+int main() {
+  Sparepart sparepart[MAX_SPAREPART];
+  User user[MAX_USER];
+
+
+  int currentSparepart = 1;
+  int currentUser = 0;
+
+  sparepart[0].name = "OLI AHM 800ML";
+  sparepart[0].stock = 100; 
+  sparepart[0].price = 150000;
+
+  while (true) {
+    // Deklarasi variabel inputUserDashboard untuk menampung pilihan user
+    int inputUserDashboard;
+
+    int attempts = 2;
+
+    // Menampilkan Menu
+    cout << "\nDASHBOARD INVENTARIS " << endl;
+    cout << "1. Register" << endl;
+    cout << "2. Login" << endl;
+    cout << "3. Keluar" << endl;
+    cout << "Masukkan pilihan Anda : ";
+    cin >> inputUserDashboard;
+
+    switch (inputUserDashboard) {
+      case 1: {
+        // Deklarasi variabel username untuk menampung username
+        string usernameRegister;
+        // Deklarasi variabel password untuk menampung password user
+        string passwordRegister;
+
+        cout << "\nREGISTER" << endl;
+
+        // Melakukan input username
+        cout << "Masukkan Username : ";
+        cin.ignore();
+        getline(cin, usernameRegister);
+
+        // Melakukan input password
+        cout << "Masukkan Password : ";
+        getline(cin, passwordRegister);
+
+        if (usernameRegister.empty() || passwordRegister.empty() ){
+          cout << "Masukkan Username dan Password yang valid!" << endl;
+        } else {
+          user[currentUser].username = usernameRegister;
+          user[currentUser].password = passwordRegister;
+          currentUser++;
+          cout << "\nRegister Berhasil!" << endl;
+        }
+      }
+      break;
+      case 2 : {
+        
+
+        while (attempts >= 0) {
+        
+          cout << "\nLogin" << endl;
+          // Deklarasi variabel username untuk menampung username
+          string usernameLogin;
+        
+          // Deklarasi variabel password untuk menampung password user
+          string passwordLogin;
+          cout << "Masukkan Username : ";
+        
+          // Input username
+          cin >> usernameLogin;
+
+          cout << "Masukkan Password : ";
+        
+          // Input password
+          cin >> passwordLogin;
+          
+          // Deklarasi variabel isExist untuk mengecek username dan password
+          bool isExist = false;
+
+          // Mengecek username dan password
+          for (int i = 0; i < currentUser; i++) {
+            if (user[i].username == usernameLogin && user[i].password == passwordLogin) {
+              // Jika username dan password ditemukan ubah variable isExist
+              isExist = true;
+            }
+          }
+          
+          if (isExist) {
+            while (true) {
+              // Menampilkan Menu
+              int inputUser;
+              cout << "\nMENU INVENTARIS " << endl;
+              cout << "1. Tambah Sparepart" << endl;
+              cout << "2. Tampilkan Sparepart" << endl;
+              cout << "3. Ubah Stok Sparepart" << endl;
+              cout << "4. Hapus Sparepart" << endl;
+              cout << "5. Keluar" << endl;
+              cout << "Masukkan pilihan Anda : ";
+              cin >> inputUser;
+            
+              switch (inputUser) {
+                case 1:
+                  // Tambah Sparepart
+                  if (currentSparepart < MAX_SPAREPART){
+                    // Deklarasi variabel inputName untuk menampung nama sparepart
+                    string inputName;
+                    // Deklarasi variabel inputQuantity untuk menampung kuantitas sparepart 
+                    int inputQuantity;
+                    // Deklarasi variabel price untuk menampung harga sparepart
+                    int inputPrice;
+
+                    // Melakukan input nama sparepart
+                    cout << "Masukkan nama sparepart : ";
+                    cin.ignore();
+                    getline(cin, inputName);
+                  
+                    // Melakukan input jumlah sparepart
+                    cout << "Masukkan jumlah sparepart : ";
+                    cin >> inputQuantity;
+                  
+                    // Melakukan input harga sparepart
+                    cout << "Masukkan total harga per item : ";
+                    cin >> inputPrice;
+                  
+                    // Melakukan validasi semua field
+                    if (inputPrice < 0 || inputQuantity < 0 || inputName.empty()) {
+                      cout << "Masukkan nama, jumlah, total harga sparepart yang valid!" << endl;
+                    } else {
+                      // Append semua field ke dalam array masing-masing
+                      sparepart[currentSparepart].name = inputName;
+                      sparepart[currentSparepart].stock = inputQuantity;
+                      sparepart[currentSparepart].price = inputPrice;
+                      currentSparepart++;
+                      cout << "\nBerhasil menambahkan sparepart!" << endl;
+                    }
+                  } else {
+                    cout << "Tidak bisa menambahkan sparepart karena inventaris sudah penuh!" << endl;
+                  }
+                  break;
+                case 2:
+                  // Tampilkan Sparepart
+                
+                  // Menampilkan header tabel
+                  cout << "+----+------------------------------------------+--------+-------------+" << endl;
+                  cout << "| No | Sparepart                                | Stok   | Harga       |" << endl;
+                  cout << "+----+------------------------------------------+--------+-------------+" << endl;
+                
+                  // Menampilkan data sparepart dalam tabel
+                  for (int i = 0; i < currentSparepart; i++) {
+                    cout << "| " << i + 1;
+                    i + 1 < 10 ? cout << "  " : cout << " ";
+                    cout << "| " << sparepart[i].name;
+                    for (int j = sparepart[i].name.length(); j < 40; j++) {
+                      cout << " ";
+                    }
+                    cout << " | " << sparepart[i].stock;
+                    if (sparepart[i].stock < 10 ) {
+                      cout << "     ";
+                    } else if (sparepart[i].stock < 100) {
+                      cout << "    ";
+                    } else {
+                      cout << "   ";
+                    }
+                    cout << " | " << sparepart[i].price;
+                    if (sparepart[i].price < 10000) {
+                      cout << "       ";
+                    } else if (sparepart[i].price < 100000) {
+                      cout << "      ";
+                    } else {
+                      cout << "     ";
+                    }
+                    cout << " |" << endl;
+                  }
+                  // Menutup tabel
+                  if (currentSparepart != 0) {
+                    cout << "+----+------------------------------------------+--------+-------------+" << endl;
+                  }
+                  break;
+                case 3:
+                  // Update Stok Sparepart
+
+                  // Mengecek apakah ada sparepart
+                  if (currentSparepart == 0) {
+                    cout << "Tidak ada sparepart yang bisa diubah!" << endl;
+                  } else {
+                    // Menampilkan header tabel
+                    cout << "+----+------------------------------------------+--------+-------------+" << endl;
+                    cout << "| No | Sparepart                                | Stok   | Harga       |" << endl;
+                    cout << "+----+------------------------------------------+--------+-------------+" << endl;
+
+                    // Menampilkan data sparepart dalam tabel
+                    for (int i = 0; i < currentSparepart; i++) {
+                      cout << "| " << i + 1;
+                      i + 1 < 10 ? cout << "  " : cout << " ";
+                      cout << "| " << sparepart[i].name;
+                      for (int j = sparepart[i].name.length(); j < 40; j++) {
+                        cout << " ";
+                      }
+                      cout << " | " << sparepart[i].stock;
+                      if (sparepart[i].stock < 10 ) {
+                        cout << "     ";
+                      } else if (sparepart[i].stock < 100) {
+                        cout << "    ";
+                      } else {
+                        cout << "   ";
+                      }
+                      cout << " | " << sparepart[i].price;
+                      if (sparepart[i].price < 10000) {
+                        cout << "       ";
+                      } else if (sparepart[i].price < 100000) {
+                        cout << "      ";
+                      } else {
+                        cout << "     ";
+                      }
+                      cout << " |" << endl;
+                    }
+                    // Menutup tabel
+                    if (currentSparepart != 0) {
+                      cout << "+----+------------------------------------------+--------+-------------+" << endl;
+                    }
+                  
+                    int indexSparepart;
+                    // Melakukan input index sparepart
+                    cout << "Masukkan No sparepart : ";
+                    cin >> indexSparepart;
+                  
+                    // Memvalidasi index sparepart
+                    if (indexSparepart > 0 && indexSparepart <= currentSparepart) {
+                      int newStcok;
+                      cout << "Masukkan stok sparepart : ";
+                      cin >> newStcok;
+                      if (newStcok < 0) {
+                        cout << "\nGagal memperbarui stok sparepart!";
+                      } else {
+                        // Mengubah stok sparepart
+                        sparepart[indexSparepart - 1].stock = newStcok;
+                        cout << "Berhasil mengubah stok!" << endl;
+                      }
+                    } else {
+                      cout << "Nomor sparepart tidak valid" << endl;
+                    }
+                  }
+
+                  break;
+                case 4:
+                  // Delete Sparepart
+                
+                  if (currentSparepart == 0) {
+                    cout << "Tidak ada sparepart yang bisa dihapus!" << endl;
+                  } else {
+                    // Menampilkan header tabel
+                    cout << "+----+------------------------------------------+--------+-------------+" << endl;
+                    cout << "| No | Sparepart                                | Stok   | Harga       |" << endl;
+                    cout << "+----+------------------------------------------+--------+-------------+" << endl;
+
+                    // Menampilkan data sparepart dalam tabel
+                    for (int i = 0; i < currentSparepart; i++) {
+                      cout << "| " << i + 1;
+                      i + 1 < 10 ? cout << "  " : cout << " ";
+                      cout << "| " << sparepart[i].name;
+                      for (int j = sparepart[i].name.length(); j < 40; j++) {
+                        cout << " ";
+                      }
+                      cout << " | " << sparepart[i].stock;
+                      if (sparepart[i].stock < 10 ) {
+                        cout << "     ";
+                      } else if (sparepart[i].stock < 100) {
+                        cout << "    ";
+                      } else {
+                        cout << "   ";
+                      }
+                      cout << " | " << sparepart[i].price;
+                      if (sparepart[i].price < 10000) {
+                        cout << "       ";
+                      } else if (sparepart[i].price < 100000) {
+                        cout << "      ";
+                      } else {
+                        cout << "     ";
+                      }
+                      cout << " |" << endl;
+                    }
+                    // Menutup tabel
+                    if (currentSparepart != 0) {
+                      cout << "+----+------------------------------------------+--------+-------------+" << endl;
+                    }
+                  
+                    int indexSparepart;
+                    // Melakukan input index sparepart
+                    cout << "Masukkan No sparepart : ";
+                    cin >> indexSparepart;
+                  
+                    // Memvalidasi index sparepart
+                    if (indexSparepart > 0 && indexSparepart <= currentSparepart) {
+                      for (int i = indexSparepart - 1; i < currentSparepart - 1; i++) {
+                        sparepart[i] = sparepart[i + 1];
+                      }
+                      currentSparepart--;
+                      cout << "Berhasil menghapus sparepart!" << endl;
+                    } else {
+                      cout << "Nomor sparepart tidak valid" << endl;
+                    }
+                  }
+                  break;
+                case 5:
+                  // Keluar dari program
+                  cout << "Keluar dari MENU INVENTARIS!" << endl;
+                  break;
+                default:
+                  // Menampilkan pesan kesalahan input pilihan
+                  cout << "Maaf, pilihan Anda tidak tersedia!" << endl;
+                  break;
+              }
+              if (inputUser == 5) {
+                break;
+              }
+
+            }
+            break;
+          } else if (attempts == 0) {
+            cout << "Kesempatan Anda memasukkan untuk login telah habis, tunggu 15 menit untuk login kembali!" << endl;
+            break;
+          } else {
+            // Menampilkan pesan kesalahan input PIN 
+            cout << "Username atau Password yang Anda masukkan salah, tersisa " << attempts;
+            cout << " percobaan" << endl;
+            attempts -= 1;
+          }
+        }
+      }
+      break;
+      case 3 :
+        // Keluar dari program
+        cout << "Keluar!" << endl;
+        break;
+      default:
+        // Menampilkan pesan kesalahan input pilihan
+        cout << "Maaf, pilihan Anda tidak tersedia!" << endl;
+        break;
+    }
+    if (inputUserDashboard == 3 || attempts == 0){
+      break;
+    }
+    
+  }
+  
+}
+
