4fe56a1d5639a6464c7d13ae34e1e18150bf4280
mfthfzn <miftahulfauzan55@gmail.com>
2025-04-27 20:29:49 +0800

    Commit Pertemuan-6

--- kelas/pertemuan-6/selectionsort.cpp
+++ kelas/pertemuan-6/selectionsort.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 using namespace std;
-void insertionSort(int a[], int panjang)
-{
-  for (int i = 1; i < panjang; i++)
-  {
-    int key = a[i];
-    int j = i - 1;
-    while (j >= 0 && a[j] > key)
-    {
-      a[j + 1] = a[j];
-      j = j - 1;
+void selectionSort(int a[], int panjang) {
+  for (int i = 0; i < panjang - 1; i++) {
+    int min = i;
+    for (int j = i + 1; j < panjang; j++) {
+      if (a[j] < a[min]){
+        min = j;
+      }
     }
-    a[j + 1] = key;
-    cout << "Tahap ke-" << i << ": ";
+    int temp = a[i];
+    a[i] = a[min];
+    a[min] = temp;
+    cout << "Tahap ke-" << i + 1 << ": ";
     for (int k = 0; k < panjang; k++)
     {
       cout << a[k] << " ";
@@ -22,8 +21,8 @@ void insertionSort(int a[], int panjang)
 }
 int main()
 {
-  int arr[6] = {7, 5, 10, 8, 32, 24};
+  int arr[6] = {8, 45, 6, 12, 81, 32};
   int panjang = sizeof(arr) / sizeof(arr[0]);
-  insertionSort(arr, panjang);
+  selectionSort(arr, panjang);
   return 0;
 }
